ce166d48c80fac74c38b358f282f02bfefb6ee6d
zyc <zyc@hasor.net>
2026-08-02 17:27:00 +0800

    Fix macOS Swift runtime bundling

--- capture-agent/capture-runtime/build.rs
+++ capture-agent/capture-runtime/build.rs
@@ -11,5 +11,8 @@ fn main() {
     #[cfg(target_os = "linux")]
     println!("cargo:rustc-link-arg=-Wl,-rpath,$ORIGIN");
     #[cfg(target_os = "macos")]
-    println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path");
+    {
+        println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path");
+        println!("cargo:rustc-link-arg=-Wl,-rpath,@loader_path/../Frameworks");
+    }
 }
